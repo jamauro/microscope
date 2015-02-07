22eeaab58f3f6f458de2d133d0a0051177ce0497
jamauro <jamauro@gmail.com>
2015-02-07 12:49:27 -0600

    Validate post contents on submission

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,12 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+
+// rendered callback triggers once template has been rendered in browser
+// this refers to the current template instance
+Template.error.rendered = function() {
+  var error = this.data;
+  Meteor.setTimeout(function() {
+    Errors.remove(error._id);
+  }, 3000);
+};
