fe1e36ae7f05ea1c760b095502af8a0e1c00f799
jamauro <jamauro@gmail.com>
2015-02-07 12:59:01 -0600

    Validate post contents when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,13 +1,30 @@
+Template.postEdit.created = function() {
+  Session.set('postEditErrors', {});
+}
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
 
     var currentPostId = this._id;
 
-    var post = {
+    var postProperties = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url)
+      return Session.set('postEditErrors', errors);
+
 
     // don't need Meteor method here if it's simple enough to do allow and deny
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
